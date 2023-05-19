e3ca373e92de4f21610d753ec4224e83338343ff
Mils7023 <milan.nariya.095@gamil.com>
2023-05-19 19:20:50 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require('dotenv')
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is running");
-})
+  res.send("API is running");
+});
 
-app.listen(5000, console.log('Server Started on Port 5000'));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+//   console.log(req.params.id);
+  const getSingleChat = chats.find((c) => c._id === req.params.id);
+  res.send(getSingleChat);
+});
+
+const PORT = process.env.PORT || 5000 ;
+
+app.listen(5000, console.log(`Server Started on Port ${PORT}`));
