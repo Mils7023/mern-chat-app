e1594bba181f2d4160a65fb18f925dbf1e36ef76
Mils7023 <milan.nariya.095@gamil.com>
2023-05-28 17:28:01 +0530

    created authantication services

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const dotenv = require('dotenv')
 const { chats } = require("./data/data");
+const ConnectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const e = require("express");
 
-const app = express();
 dotenv.config();
+ConnectDB();
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
 
-app.get("/api/chat/:id", (req, res) => {
-//   console.log(req.params.id);
-  const getSingleChat = chats.find((c) => c._id === req.params.id);
-  res.send(getSingleChat);
-});
+// app.get("/api/chat/:id", (req, res) => {
+// //   console.log(req.params.id);
+//   const getSingleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(getSingleChat);
+// });
+
+app.use('api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000 ;
 
